105bed1a18c668312535a75c189ec09565ad8486
q1w2e3r4 <211250228@smail.nju.edu.cn>
2024-10-12 12:56:08 +0800

    + .gitignore

--- aaa.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-char month[20], weekday[20];
-int day,year,h,m,s;
-
-int main(){
-    scanf("%s %d %d %s %d %d %d",month,&day,&year,weekday,&h,&m,&s);
-    printf("%.3s %.3s %02d %02d:%02d:%02d %04d",weekday, month, day,h,m,s,year);
-}
